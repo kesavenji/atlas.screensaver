02a4e184306015b74b5ce5952539eec6175f586e
fezcode <samil.bulbul@gmail.com>
2026-03-04 00:48:26 +0300

    Bump v0.3.1: add --help flag

--- main.go
+++ main.go
@@ -19,6 +19,15 @@ func main() {
 		fmt.Printf("atlas.screensaver v%s\n", Version)
 		return
 	}
+	if len(os.Args) > 1 && (os.Args[1] == "-h" || os.Args[1] == "--help" || os.Args[1] == "help") {
+		fmt.Println("Atlas Screensaver - A collection of aesthetic terminal screensavers.")
+		fmt.Println("\nUsage:")
+		fmt.Println("  atlas.screensaver        Start a random screensaver")
+		fmt.Println("  atlas.screensaver [name] Start a specific screensaver (matrix, stars, pipes, bouncing)")
+		fmt.Println("  atlas.screensaver -v     Show version")
+		fmt.Println("  atlas.screensaver -h     Show this help")
+		return
+	}
 
 	p := tea.NewProgram(ui.NewModel(), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
